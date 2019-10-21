84a2b65ce4c46fc2310de6b8e200486535559acd
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-10-21 19:08:51 +0300

    HESHAM AND FAY

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from .models import Flight, Booking
 
@@ -25,4 +26,26 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['date', 'passengers']
+
+class RegisterSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['username','password','last_name','first_name']
+
+	def create(self, validated_data):
+		username_steve = validated_data.get("username")
+		password_steve = validated_data.get("password")
+
+		last_name_steve = validated_data.get("last_name")
+		first_name_steve = validated_data.get("first_name")
+
+		new_user = User(username=username_steve, last_name=last_name_steve,first_name=first_name_steve)
+		
+		new_user.set_password(password_steve)
+
+		new_user.save()
+		return validated_data
+	
+
+
 
